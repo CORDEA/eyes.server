e8cecfe5a6ae4c8ad65630f93cb3804ea7134535
Yoshihiro Tanaka <contact@cordea.jp>
2019-09-19 21:14:22 +0900

    define response

---
+++ src/client/response.rs
@@ -0,0 +1,32 @@
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct Response {
+    results: Vec<Result>,
+}
+
+#[derive(Deserialize)]
+pub struct Result {
+    address_components: Vec<AddressComponent>,
+    formatted_address: String,
+    geometry: Geometry,
+}
+
+#[derive(Deserialize)]
+pub struct AddressComponent {
+    long_name: String,
+    short_name: String,
+    types: Vec<String>,
+}
+
+#[derive(Deserialize)]
+pub struct Geometry {
+    location: Location,
+    location_type: String,
+}
+
+#[derive(Deserialize)]
+pub struct Location {
+    lat: f32,
+    lng: f32,
+}
