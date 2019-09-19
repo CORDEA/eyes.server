01414be12b318f940ddd9d8d8a5ae097f85b4ccc
Yoshihiro Tanaka <contact@cordea.jp>
2019-09-19 12:06:35 +0900

    define new route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
+use rocket_contrib::json::Json;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct LatLng {
+    latitude: String,
+    longitude: String,
+}
+
+#[post("/latlng", data = "<latlng>")]
+fn latlng(latlng: Json<LatLng>) -> &'static str {
+    ""
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite()
+        .mount("/", routes![latlng])
+        .launch();
 }
