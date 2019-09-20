3bf7cb298acf34244d6b803f736e564303ee3dc8
Yoshihiro Tanaka <contact@cordea.jp>
2019-09-20 16:31:29 +0900

    define request

--- src/slack/mod.rs
+++ src/slack/mod.rs
@@ -1,5 +1,18 @@
+use serde::Deserialize;
 
 const URL: &str = "https://slack.com/api/users.profile.set";
+
+#[derive(Deserialize)]
+pub struct Request {
+    profile: Profile
+}
+
+#[derive(Deserialize)]
+struct Profile {
+    status_text: String,
+    status_emoji: String,
+    status_expiration: i32,
+}
 
 pub struct Client {
     key: String,
