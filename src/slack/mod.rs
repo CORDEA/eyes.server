f07e12ad187fc39a5020358672657de0317d698e
Yoshihiro Tanaka <contact@cordea.jp>
2019-09-20 16:56:21 +0900

    implement

--- src/slack/mod.rs
+++ src/slack/mod.rs
@@ -1,13 +1,13 @@
-use serde::Deserialize;
+use serde::Serialize;
 
 const URL: &str = "https://slack.com/api/users.profile.set";
 
-#[derive(Deserialize)]
+#[derive(Serialize)]
 pub struct Request {
     profile: Profile
 }
 
-#[derive(Deserialize)]
+#[derive(Serialize)]
 struct Profile {
     status_text: String,
     status_emoji: String,
@@ -19,7 +19,20 @@ pub struct Client {
 }
 
 impl Client {
-
+    pub fn request(&self, text: &str) -> Result<reqwest::Response, reqwest::Error> {
+        let client = reqwest::Client::new();
+        let profile = Request {
+            profile: Profile {
+                status_text: text.to_owned(),
+                status_emoji: String::new(),
+                status_expiration: 0,
+            }
+        };
+        client.post(URL)
+            .bearer_auth(&self.key)
+            .json(&profile)
+            .send()
+    }
 }
 
 pub fn new_client(key: String) -> Client {
